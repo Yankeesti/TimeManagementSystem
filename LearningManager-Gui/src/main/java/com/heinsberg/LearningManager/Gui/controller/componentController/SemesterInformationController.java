9d9615fdb471765849dadd7503cd62d529097034
Yankeesti <timheinsberg@web.de>
2023-01-27 14:21:21 +0100

    Add Subject through Gui
    -Subject is addable through the GUI
    -TreeView gets automaticlly updated

--- LearningManager-Gui/src/main/java/com/heinsberg/LearningManager/Gui/controller/componentController/SemesterInformationController.java
+++ LearningManager-Gui/src/main/java/com/heinsberg/LearningManager/Gui/controller/componentController/SemesterInformationController.java
@@ -5,19 +5,41 @@ import com.heinsberg.LearningManager.Gui.controller.BaseController;
 import com.heinsberg.LearningManager.Gui.view.ViewFactory;
 import com.heinsberg.LearningManagerProjekt.BackGround.Study;
 import com.heinsberg.LearningManagerProjekt.BackGround.TimeClasses.Semester;
+import com.heinsberg.LearningManagerProjekt.BackGround.subject.Subject;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class SemesterInformationController extends BaseInformationComponentController{
+public class SemesterInformationController extends BaseInformationComponentController implements Initializable{
     @FXML
     Label titleLabel;
+    @FXML
+    Button createSubjectButton;
+    @FXML
+    Spinner<Integer> ectsPointPicker;
+    @FXML
+    TextField subjectNameChooser;
     private Semester semester;
     public SemesterInformationController(StudyManager studyManager, ViewFactory viewFactory, String fxmlName) {
         super(studyManager, viewFactory, fxmlName);
+    }
+
+    @FXML
+    void createSubjectAction(){
+        Subject newSubject = getSubject();
+        if(newSubject != null)
+        studyManager.addNewSubject(newSubject);
+    }
+
+    private Subject getSubject() {
+        if(!subjectNameChooser.getText().isEmpty()){
+            return new Subject(subjectNameChooser.getText(),semester,ectsPointPicker.getValue());
+        }
+        return null;
     }
 
     @Override
@@ -36,4 +58,14 @@ public class SemesterInformationController extends BaseInformationComponentContr
         titleLabel.setText(semester.getSemester() +". Semester");
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setUpEctsPointPicker();
+    }
+
+    private void setUpEctsPointPicker() {
+        SpinnerValueFactory spinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,Integer.MAX_VALUE);
+        spinnerValueFactory.setValue(1);
+        ectsPointPicker.setValueFactory(spinnerValueFactory);
+    }
 }
--- LearningManager-Gui/src/main/java/com/heinsberg/LearningManager/Gui/treeItems/SemesterTreeItem.java
+++ LearningManager-Gui/src/main/java/com/heinsberg/LearningManager/Gui/treeItems/SemesterTreeItem.java
@@ -3,24 +3,49 @@ package com.heinsberg.LearningManager.Gui.treeItems;
 import com.heinsberg.LearningManager.Gui.view.ViewFactory;
 import com.heinsberg.LearningManagerProjekt.BackGround.TimeClasses.Semester;
 import com.heinsberg.LearningManagerProjekt.BackGround.subject.Subject;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
+
+import java.util.List;
 
 public class SemesterTreeItem<String> extends BaseTreeItem<String> {
     private Semester semester;
+    private ObservableList<Subject> subjects;
 
     public SemesterTreeItem(Semester semester) {
-        super((String) (semester.getSemester()+". Semester"));
+        super((String) (semester.getSemester() + ". Semester"));
         this.semester = semester;
         setUpSubjects();
         setExpanded(true);
     }
 
     private void setUpSubjects() {
-        Subject[] subjects = semester.getSubjects();
-        for(Subject subject:subjects){
+        subjects = semester.getSubjects();
+        for (Subject subject : subjects) {
             SubjectTreeItem subjectTreeItem = new SubjectTreeItem(subject);
             getChildren().add(subjectTreeItem);
         }
+        //Add Listener to Subjects
+        subjects.addListener(new ListChangeListener<Subject>() {
+            @Override
+            public void onChanged(Change<? extends Subject> change) {
+                change.next();
+                if(change.wasAdded()){ //add new Semester to tree
+                    //get the last addedObject
+                    List<? extends Subject> addedList = change.getAddedSubList();
+
+                    addSubjectTreeItem((Subject)addedList.get(addedList.size()-1));
+                }else if (change.wasRemoved()){
+
+                }
+            }
+        });
+    }
+
+    private void addSubjectTreeItem(Subject subject) {
+        SubjectTreeItem newTreeItem = new SubjectTreeItem(subject);
+        getChildren().add(newTreeItem);
     }
 
     @Override
